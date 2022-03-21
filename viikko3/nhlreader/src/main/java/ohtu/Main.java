0fa9a1a88caa95245be8a7ce14da12e6942a9f5c
Pasi Tappola <pasi.tappola@luukku.com>
2022-03-21 21:53:09 +0200

    Teht 1 	modified:   build.gradle 	modified:   src/main/java/ohtu/Main.java 	modified:   src/main/java/ohtu/Player.java

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -5,10 +5,30 @@
  */
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
 /**
  *
- * @author mluukkai
+ * @author ptappola
  */
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+//        System.out.println("json-muotoinen data:");
+//        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        System.out.println("Suomalaiset pelaajat:");
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+                System.out.println(player);
+            }
+        }   
+    }
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,18 +3,25 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private String team;
 
     public void setName(String name) {
         this.name = name;
     }
-
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public String getNationality() {
+        return this.nationality;
+    }
     public String getName() {
         return name;
     }
 
     @Override
     public String toString() {
-        return name;
+        return name + " " + team;
     }
       
 }
