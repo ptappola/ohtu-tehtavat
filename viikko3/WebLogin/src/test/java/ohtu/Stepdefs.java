50010d0122c263274507cf4a08b9d0f309c05747
Pasi Tappola <pasi.tappola@luukku.com>
2022-03-23 15:13:09 +0200

    Teht 10 	modified:   src/main/java/ohtu/authentication/AuthenticationService.java 	modified:   src/test/java/ohtu/Stepdefs.java 	modified:   src/test/resources/ohtu/logging_in.feature

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -22,6 +22,13 @@ public class Stepdefs {
         WebElement element = driver.findElement(By.linkText("login"));       
         element.click();   
     }
+
+    @Given("command new user is selected")
+    public void newUserIsSelected() {
+	driver.get(baseUrl);
+	WebElement element = driver.findElement(By.linkText("register new user"));
+	element.click();
+    }
     
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
@@ -42,13 +49,36 @@ public class Stepdefs {
     public void incorrectUsernameAndIncorrectPasswordAreGiven(String username, String password) {
 	logInWith(username, password);
     }
-    
+
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void newUsernameAndPasswordAreGiven(String username, String password) {
+	createUser(username, password, password);
+    }
+    @When("a invalid username {string} and password {string} and matching password confirmarion are entered")
+    public void newInvalidUsernameAndPasswordAreGiven(String username, String password) {
+	createUser(username, password, password);
+    }
+    @When("a valid username {string} and invalid password {string} are entered")
+    public void newUsernameAndInvalidPasswordAreGiven(String username, String password) {
+	createUser(username, password, password);
+    }
+    @When("a valid username {string} and valid password {string} and password {string} are given")
+    public void newUsernameAndTwoPasswordsAreGiven(String username, String password1, String password2) {
+	createUser(username, password1, password2);
+    }
+    @Then("a new user is created")
+    public void newUserIsCreated() {
+	pageHasContent("Welcome to Ohtu Application!");
+    }
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
     }    
-    
+    @Then("user is not created and error {string} is reported")
+    public void userIsNotCreated(String errorCode) {
+	pageHasContent(errorCode);
+    }
     @When("username {string} and password {string} are given")
     public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
         logInWith(username, password);
@@ -65,7 +95,16 @@ public class Stepdefs {
     }
         
     /* helper methods */
- 
+    private void createUser(String username, String password1, String password2) {
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password1);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password2);
+        element = driver.findElement(By.name("signup"));
+	element.submit();
+    }
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
     }
